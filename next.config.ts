e8715db6da4e5ea8abf09f7db6a5a3ca7300eed8
slkzgm <contact@slkzgm.com>
2025-04-11 13:57:24 +0200

    fix next.config.ts logs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'production' ? { exclude: ['error', 'warn'] } : false,
+    removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error', 'warn'] } : false,
   },
 }
 
